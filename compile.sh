6bf01ca27eda20ff3c160bfa118fa968085d8dd8
Judicaël Grasset <judicael.grasset@meteo.fr>
2025-03-12 16:06:03 +0100

    always clean before compiling"

--- compile.sh
+++ compile.sh
@@ -4,6 +4,10 @@ set -xeu
 
 module load rocm
 
+#cleaning
+find -name "*.o" -delete
+find -name "*.mod" -delete
+
 FC_FLAGS="-DWITH_FIAT  -I/home/grassetj/install/include -I/home/grassetj/install/include/fiat -I/home/grassetj/install/module/fiat -I/home/grassetj/install/module/parkind_dp -O2 -g -fPIC -fopenmp -c"
 FC="amdflang $FC_FLAGS"
 
@@ -79,9 +83,5 @@ $FC factory/field_1rm_factory_module.F90
 $FC factory/field_4lm_factory_module.F90
 $FC factory/field_4rm_factory_module.F90
 $FC factory/field_factory_module.F90
-
-$FC util/field_2im_access_module.F90
-$FC util/field_3im_access_module.F90
-$FC util/field_access_module.F90
 
 $FC shuffle/field_shuffle_type_module.F90
