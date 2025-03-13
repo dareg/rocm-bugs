df12d16fbed35689d4cf6b72147ba56b1e5677e7
Judicaël Grasset <judicael.grasset@meteo.fr>
2025-03-13 10:17:09 +0100

    reduce

--- compile.sh
+++ compile.sh
@@ -10,14 +10,6 @@ module load rocm
 FC_FLAGS="-DWITH_FIAT  -I/home/grassetj/install/include -I/home/grassetj/install/include/fiat -I/home/grassetj/install/module/fiat -I/home/grassetj/install/module/parkind_dp -O2 -g -fPIC -fopenmp -c"
 FC="amdflang $FC_FLAGS"
 
-$FC debug/field_statistics_module.F90
-$FC core/field_constants_module.F90
-$FC core/field_abort_module.F90
-$FC core/field_defaults_module.F90
-$FC core/host_alloc_module.F90
-$FC core/dev_alloc_module.F90
-$FC core/field_basic_module.F90
-
 
 $FC factory/field_2im_factory_module.F90
 $FC factory/field_3im_factory_module.F90
